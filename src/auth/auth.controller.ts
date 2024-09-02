56d3d9e3b489db80c7ffcec417b9ea56cac0f465
bill7maxmaye <bilenmehalek7@gmail.com>
2024-09-02 10:57:26 +0300

    commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller,  Post } from '@nestjs/common';
+import { Body, Controller,  Post, ValidationPipe } from '@nestjs/common';
 import { AuthCredetialsDto } from './dto/auth-credentials.dto';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
@@ -8,7 +8,7 @@ export class AuthController {
   constructor( private authService: AuthService) {}
 
   @Post('/signup')
-  SignUpUser(@Body() authCredentialDto: AuthCredetialsDto): Promise<User> {
+  SignUpUser(@Body(ValidationPipe) authCredentialDto: AuthCredetialsDto): Promise<User> {
     return this.authService.SignUpUser(authCredentialDto);
   }
 }
--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,5 +1,19 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
 
-export class AuthCredetialsDto{
-    username: string;
-    password: string;
+export class AuthCredetialsDto {
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])$/,
+    {
+      message: 'password too weak',
+    },
+  )
+  password: string;
 }
